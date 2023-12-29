2072f85864289d103410209433078a9da07f2c81
Peter John Acklam <pjacklam@gmail.com>
2023-12-29 14:51:56 +0100

    Version 1.7000

--- lib/Math/BigInt/GMP.pm
+++ lib/Math/BigInt/GMP.pm
@@ -8,7 +8,7 @@ use Math::BigInt::Lib 1.999801;
 
 our @ISA = qw< Math::BigInt::Lib >;
 
-our $VERSION = '1.6014';
+our $VERSION = '1.7000';
 
 use XSLoader;
 XSLoader::load "Math::BigInt::GMP", $VERSION;
